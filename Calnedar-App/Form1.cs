810a07460da72c950d2deee89976dfbd7daa8d51
Md. Jubayer Hossain Abir <41603187+Jubayer-Hossain-Abir-404@users.noreply.github.com>
2021-11-23 07:03:24 +0600

    Initial commit

---
+++ Calnedar-App/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calnedar_App
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            displayDays();
+        }
+
+        private void displayDays()
+        {
+            DateTime now = DateTime.Now;
+
+            //Lets get the firts day of the month
+
+
+            DateTime startofthemonth = new DateTime(now.Year, now.Month, 1);
+            //get the count of days of the month
+            int days = DateTime.DaysInMonth(now.Year, now.Month);
+            // convert the startof the month to integer
+            int dayoftheweek = Convert.ToInt32(startofthemonth.DayOfWeek.ToString("d"));
+
+            //first a blank usercontrol will be created
+            //for()
+        }
+
+        private void button_Previous_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button_exit_Click(object sender, EventArgs e)
+        {
+            DialogResult = DialogResult.OK;
+            Close();
+        }
+    }
+}
